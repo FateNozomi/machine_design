69518e84067e3fc04f8eeaf87b9469ec34007bdc
L <L>
2016-09-24 16:31:22 +0800

    Add winding layout output feature

--- designMotor.m
+++ designMotor.m
@@ -1,4 +1,4 @@
-function s = designMotor()
+function final = designMotor()
 polesPrompt = 'Enter number of poles: ';
 slotsPrompt = 'Enter number of slots: ';
 nomCoilSpanPrompt = 'Enter nominal coil span: ';
@@ -6,6 +6,9 @@ POLES = input(polesPrompt);
 SLOTS = input(slotsPrompt);
 nomCoilSpan = input(nomCoilSpanPrompt);
 
+% POLES = 10;
+% SLOTS = 12;
+% nomCoilSpan = 1;
 % find relative angle of all potential coils
 relativeAngle = POLES / SLOTS * 180;
 
@@ -66,12 +69,43 @@ phaseA_abs = zeros(1,slotsPerPhase); %Array to temporary store Phase A values
 % due to the previous index being removed.
 k = 0;
 z = s; %Assign 's' into a new array 'z' so that 's' remains untouched.
-for i = 1:slotsPerPhase
-    %
-    [phaseA_abs(i),index] = min(abs(z(1,i:end)));
-    
+zAngle = z(1,:); %Get the angle row
+% All zAngle negative values are increased by 1 so that the min index will
+% always get negative values first. (Quick hack)
+zAngle(zAngle<0) = (zAngle(zAngle<0)+1);
+
+for i = 1:SLOTS
+    % index returns the position of the minimum value
+    [phaseA_abs(i),index] = min(abs(zAngle(1,i:end)));
+    % reposition zAngle from min to max
+    zAngle(:,[i index+k]) = zAngle(:,[index+k i]);
+    % reposition z from min to max
     z(:,[i index+k]) = z(:,[index+k i]);
     
-    k = k + 1;
+    k = k + 1; %increase k to compensate shrinking zAngle and z
 end
+
+phase = zeros(2,slotsPerPhase,3);
+phaseOffset = SLOTS-slotsPerPhase;
+
+%Assign Phase A
+phaseA = z([2 3],1:slotsPerPhase);
+phase(:,:,1) = phaseA;
+
+% Assign Phase B
+phaseB = phaseA + (phaseOffset);
+%Minus SLOTS if more than SLOTS
+phaseB(phaseB>SLOTS) = (phaseB(phaseB>SLOTS)-SLOTS);
+phase(:,:,2) = phaseB;
+
+% Assign Phase C
+phaseC = phaseB + (phaseOffset);
+%Minus SLOTS if more than SLOTS
+phaseC(phaseC>SLOTS) = (phaseC(phaseC>SLOTS)-SLOTS);
+phase(:,:,3) = phaseC;
+
+% Assign 's' and 'phase' into a cell array
+final = {s phase};
+fprintf('To view final angle, enter: s = ans{1}\n');
+fprintf('To view winding layout, enter: phase = ans{2}');
 end
